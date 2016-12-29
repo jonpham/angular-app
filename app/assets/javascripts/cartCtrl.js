dd8f358ab433d90a992a316c2dc7af5529b861b6
Jonathan Pham <jon.pham@gmail.com>
2016-12-28 21:24:55 -0800

    Add Controller JavaScript.

---
+++ app/assets/javascripts/cartCtrl.js
@@ -0,0 +1,18 @@
+(function() {
+  "use strict";
+
+  angular.module("app").controller("cartCtrl", 
+    function($scope)
+    {
+      // DEBUG
+      window.$scope = $scope;
+
+      // Global Variables
+
+      // Function Code
+
+      // Page Execution 
+    }
+  );
+
+})();
